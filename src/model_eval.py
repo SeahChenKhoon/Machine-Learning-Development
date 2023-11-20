9dbed9fee1c96bf837d12e8e86c28f7f2b5a1704
SeahChenKhoon <chenkhoon@yahoo.com>
2023-11-20 12:42:08 +0800

    Refactor the init and print_report to print confusion matrix and classification report

--- src/model_eval.py
+++ src/model_eval.py
@@ -1,52 +1,45 @@
 import pandas as pd
 from sklearn.metrics import accuracy_score, classification_report, ConfusionMatrixDisplay, \
-    precision_score, recall_score, f1_score, roc_auc_score, roc_curve
+    precision_score, recall_score, f1_score, roc_auc_score, roc_curve, confusion_matrix
 
 class ModelEval:
-    def __init__(self, model_type:str, y_train:pd.DataFrame, y_train_pred:pd.DataFrame) -> None:
+    def __init__(self, model_type:str, y:pd.DataFrame, y_pred:pd.DataFrame) -> None:
         """
-        Initialize an instance of the class.
-
-        This constructor initializes an instance of the class with model type and performance metrics
-        based on the training data.
+        Initializes an instance of the class with the specified parameters.
 
         Parameters:
-            model_type (str): The type of the model.
-            y_train (pd.DataFrame): The true target values for the training data.
-            y_train_pred (pd.DataFrame): The predicted target values for the training data.
+        - model_type (str): The type of the model as a string.
+        - y (pd.DataFrame): The actual labels (ground truth) as a pandas DataFrame.
+        - y_pred (pd.DataFrame): The predicted labels as a pandas DataFrame.
 
         Returns:
-            None: This constructor does not return a value.
+        None
         """
         self.model_type = model_type.upper()
-        self.model_train_accuracy = accuracy_score(y_train, y_train_pred)
-        self.model_train_f1 = f1_score(y_train, y_train_pred, average="weighted")
-        self.model_train_precision = precision_score(y_train, y_train_pred, average="weighted")
-        self.model_train_recall = recall_score(y_train, y_train_pred, average="weighted")
+
+        self.confusion_matrix_test = confusion_matrix(y, y_pred)
+        self.class_rpt_test = classification_report(y, y_pred)
         return None
 
     def print_report(self, report_type:str="Training")-> None:
         """
-        Print a performance report for the model.
-
-        This method prints a performance report for the model, including accuracy, F1 score,
-        precision, and recall.
+        Print the performance report for the specified set (Training or Testing).
 
         Parameters:
-            report_type (str, optional): The type of report to print, "Training" (default) or "Testing".
+        - report_type (str, optional): The type of the report to be printed. Defaults to "Training".
 
         Returns:
-            None: This method does not return a value.
+        None
         """
         print(f"{self.model_type}")
         if report_type == "Training":
             print(f"Model Performance for Training set")
         else:
             print(f"Model Performance for Testing set")
-        print(" - Accuracy: {:.4f}".format(self.model_train_accuracy))
-        print(" - F1 score: {:.4f}".format(self.model_train_f1))
-        print(" - Precision: {:.4f}".format(self.model_train_precision))
-        print(" - Recall: {:.4f}".format(self.model_train_recall))
+        print('Confusion_matrix')
+        print(self.confusion_matrix_test)
+        print('\nClassification Report')
+        print(self.class_rpt_test)
         print('='*35)
         print('\n')
         return None
