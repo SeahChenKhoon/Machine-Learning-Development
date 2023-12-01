98c7a29f0a08315e633be61e8ebee144823c01ca
SeahChenKhoon <chenkhoon@yahoo.com>
2023-12-01 10:58:28 +0800

    Create DB class Read yaml from YAML file Read data from Pre_cruise Read data from Post_Cruise

---
+++ src/database.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import sqlite3
+import util
+
+class Database():
+    def __init__(self, data_path):
+        self._data_path = data_path
+        self._dataframe = pd.DataFrame()
+        return None
+
+
+    def db_read(self, database:list)->pd.DataFrame:
+        try:
+            connection:connection = sqlite3.connect(self._data_path   + database['file_name'])
+            dataframe:pd.DataFrame = pd.read_sql_query("SELECT * FROM " + database['table_name'], connection)
+            return dataframe.set_index(database['index']).drop_duplicates()
+        except sqlite3.Error as e:
+            print("SQLite error:", e)
+            return None
+        
--- src/util.py
+++ src/util.py
@@ -4,74 +4,87 @@ import yaml
 from datetime import datetime
 from typing import Any
 
-def timer(start_time=datetime)->datetime:
-    """
-    Measure and print the elapsed time.
+def read_yaml(yaml_filepath:str):
+    # Read the YAML file
+    with open(yaml_filepath, 'r') as file:
+        data = yaml.safe_load(file)
+        return data
 
-    This function calculates and prints the elapsed time since the provided `start_time`.
+def merge_dataframe(df_pre_cruise, df_post_cruise):
+    return pd.merge(df_pre_cruise, df_post_cruise, left_index=True, right_index=True, how='inner')
+    
+def print_type_value(data):
+    print(type(data))
+    print(data)
 
-    Parameters:
-        start_time (datetime, optional): The starting time to calculate the elapsed time from.
-            If not provided, the current time will be used as the starting time.
+# def timer(start_time=datetime)->datetime:
+#     """
+#     Measure and print the elapsed time.
 
-    Returns:
-        datetime: The `start_time` when provided, or the current time if `start_time` is None.\
-    """
-    if not start_time:
-        start_time = datetime.now()
-        return start_time
-    elif start_time:
-        thour, temp_sec = divmod((datetime.now() - start_time).total_seconds(), 3600)
-        tmin, tsec = divmod(temp_sec, 60)
-        print("\n Time taken: %i hours %i minutes and %s seconds." % (thour, tmin, round(tsec,2)))
-        return
+#     This function calculates and prints the elapsed time since the provided `start_time`.
 
-def output_csv (data_path:str,dataframe:pd.DataFrame,dateframe_name:str)->None:
-    """
-    This function output a csv from a dataframe and store in data folder
+#     Parameters:
+#         start_time (datetime, optional): The starting time to calculate the elapsed time from.
+#             If not provided, the current time will be used as the starting time.
 
-    Parameters:
-        col_name (str): Output filename of the csv
-        dataframe (pd.DataFrame): Dataset in which to output to csv
+#     Returns:
+#         datetime: The `start_time` when provided, or the current time if `start_time` is None.\
+#     """
+#     if not start_time:
+#         start_time = datetime.now()
+#         return start_time
+#     elif start_time:
+#         thour, temp_sec = divmod((datetime.now() - start_time).total_seconds(), 3600)
+#         tmin, tsec = divmod(temp_sec, 60)
+#         print("\n Time taken: %i hours %i minutes and %s seconds." % (thour, tmin, round(tsec,2)))
+#         return
 
-    Returns:
-        dataframe (pd.DataFrame): Return back the processed dataset    
-    """
-    timestamp = datetime.now().strftime("%Y%m%d%H%M")
-    filename = f"{data_path}{dateframe_name}_{timestamp}.csv"
-    dataframe.to_csv(filename)
-    return
+# def output_csv (data_path:str,dataframe:pd.DataFrame,dateframe_name:str)->None:
+#     """
+#     This function output a csv from a dataframe and store in data folder
 
-def drop_columns(dataframe:pd.DataFrame, col_names:list)->pd.DataFrame:
-    """
-    Drop specified columns from a pandas DataFrame.
+#     Parameters:
+#         col_name (str): Output filename of the csv
+#         dataframe (pd.DataFrame): Dataset in which to output to csv
 
-    Parameters:
-    - dataframe (pd.DataFrame): The DataFrame from which columns will be dropped.
-    - col_names (list of str): A list of column names to be dropped from the DataFrame.
+#     Returns:
+#         dataframe (pd.DataFrame): Return back the processed dataset    
+#     """
+#     timestamp = datetime.now().strftime("%Y%m%d%H%M")
+#     filename = f"{data_path}{dateframe_name}_{timestamp}.csv"
+#     dataframe.to_csv(filename)
+#     return
 
-    Returns:
-    - pd.DataFrame: A new DataFrame with the specified columns removed.
-    """
-    for col_name in col_names:
-        dataframe.drop(col_name, axis=1,inplace=True)
-    return dataframe
+# def drop_columns(dataframe:pd.DataFrame, col_names:list)->pd.DataFrame:
+#     """
+#     Drop specified columns from a pandas DataFrame.
 
-def read_config_file(config_file_path: str)-> Any:
-    """
-    Read and load configuration data from a YAML file.
+#     Parameters:
+#     - dataframe (pd.DataFrame): The DataFrame from which columns will be dropped.
+#     - col_names (list of str): A list of column names to be dropped from the DataFrame.
 
-    This function reads and loads configuration data from a YAML file located at the specified
-    `config_file_path`.
+#     Returns:
+#     - pd.DataFrame: A new DataFrame with the specified columns removed.
+#     """
+#     for col_name in col_names:
+#         dataframe.drop(col_name, axis=1,inplace=True)
+#     return dataframe
 
-    Parameters:
-        config_file_path (str): The path to the YAML configuration file.
+# def read_config_file(config_file_path: str)-> Any:
+#     """
+#     Read and load configuration data from a YAML file.
 
-    Returns:
-        Any: The loaded configuration data. The data type may vary depending on the content of
-        the YAML file.
-    """
-    with open(config_file_path, 'r') as file:
-        config = yaml.safe_load(file)
-    return config
+#     This function reads and loads configuration data from a YAML file located at the specified
+#     `config_file_path`.
+
+#     Parameters:
+#         config_file_path (str): The path to the YAML configuration file.
+
+#     Returns:
+#         Any: The loaded configuration data. The data type may vary depending on the content of
+#         the YAML file.
+#     """
+#     with open(config_file_path, 'r') as file:
+#         config = yaml.safe_load(file)
+#     return config
 
