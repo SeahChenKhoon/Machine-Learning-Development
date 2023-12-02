6a42f410f0fc9146fd3227436016cf888247948b
SeahChenKhoon <chenkhoon@yahoo.com>
2023-12-02 17:37:37 +0800

    Implement Age

--- src/feature_engineering.py
+++ src/feature_engineering.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
+import util
 
 def plot_corr_chart(df_dataframe: pd.DataFrame)->None:
     corr_matrix = df_dataframe.corr()
@@ -12,6 +13,15 @@ def plot_corr_chart(df_dataframe: pd.DataFrame)->None:
     sns.heatmap(corr_matrix, annot=True, mask=mask, vmin=-1, vmax=1, cmap="coolwarm")
     plt.title('Correlation Coefficient Of Predictors')
     plt.show()
+
+def convert_miles_to_KM (df_dataframe: pd.DataFrame, col_name:str)->None:
+    df_dataframe[col_name] *= 1.609344
+    return None
+
+def calc_year_diff (df_dataframe: pd.DataFrame, minuend_col:str, subtrahend_col:str, new_col:str)->None:
+    df_dataframe[new_col] = df_dataframe[minuend_col] - df_dataframe[subtrahend_col]
+    util.remove_col(df_dataframe, [minuend_col, subtrahend_col])
+    return None
 
 # class FeatureEngineer:
 #     def feature_grouping(self, merged_data:pd.DataFrame, column_list:list[str], column_grp:str)->None:
